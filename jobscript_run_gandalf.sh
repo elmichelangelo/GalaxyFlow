dd0431faca60bb8791b787908fe2531e2d77d5ef
p.gebhardt <p.gebhardt@lmu.de>
2024-11-08 19:27:39 +0100

    - work on paper plots on LMU cluster

--- jobscript_run_gandalf.sh
+++ jobscript_run_gandalf.sh
@@ -5,10 +5,11 @@
 #SBATCH --time=12:00:00
 #SBATCH --mem=256G
 #SBATCH --chdir=/home/p/P.Gebhardt/development/GalaxyFlow/
-#SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF_paper/run_gaNdalF.log
-#SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF_paper/run_gaNdalF_err.log
-#SBATCH --partition=cip
-#SBATCH --gres=gpu:a40:0
+#SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF/run_gaNdalF.log
+#SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF/run_gaNdalF_err.log
+##SBATCH --partition=cip
+#SBATCH --partition=inter
+##SBATCH --gres=gpu:a40:0
 #SBATCH --cpus-per-task=8
 
 ##module load python/3.11-2024.06
