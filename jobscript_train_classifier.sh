114f778c54efc1c3acec1a3052c10c5486dec0d9
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-03-11 15:39:00 +0100

    - compare cpu vs cuda

--- jobscript_train_classifier.sh
+++ jobscript_train_classifier.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=train_gandalf_classifier
+#SBATCH --job-name=train_gandalf_classifier_cpu_cuda_test
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
 #SBATCH --mem=128G
 #SBATCH --chdir=/home/p/P.Gebhardt/development/GalaxyFlow/
-#SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF_train_classifer/gandalf_train_classifer_new.log
-#SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF_train_classifer/gandalf_train_classifer_new_err.log
+#SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF_train_classifer/gandalf_train_classifer_cpu_cuda.log
+#SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF_train_classifer/gandalf_train_classifer_cpu_cuda_err.log
 #SBATCH --partition=inter
 #SBATCH --gres=gpu:a40:1
 #SBATCH --cpus-per-task=8
