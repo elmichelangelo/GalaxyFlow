c865357cba4e13c6873b5deccc02b6b8e32191cf
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-05-19 09:55:58 +0200

    Add LoggerHandler import to Handler module
    Imported `LoggerHandler` to enable centralized logging functionality within the Handler module, facilitating better traceability and debugging. This change ensures consistent use of logging utilities across different components.

--- Handler/__init__.py
+++ Handler/__init__.py
@@ -3,3 +3,5 @@ from . helper_functions import *
 from . plot_functions import *
 from . healpy_functions import load_healpix, match_skybrite_2_footprint
 import Handler.flow as fnn
+
+from . logger import LoggerHandler
