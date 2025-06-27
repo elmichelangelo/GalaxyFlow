32b88dfac1bac1097b406daa43a2f7680550a66c
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-06-27 10:20:21 +0200

    change jobscript

--- jobscript_train_classifier.sh
+++ jobscript_train_classifier.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=weight_subset_train_gandalf_classifier_run
+#SBATCH --job-name=classifier_training
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
 #SBATCH --mem=128G
 #SBATCH --chdir=/home/p/P.Gebhardt/development/GalaxyFlow/
-#SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF_train_classifer/gandalf_train_classifer_weight.log
-#SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF_train_classifer/gandalf_train_classifer_weight_err.log
+#SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF_train_classifer/gandalf_train_classifer_new_data.log
+#SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF_train_classifer/gandalf_train_classifer_new_data_err.log
 #SBATCH --partition=inter
 #SBATCH --gres=gpu:a40:1
 #SBATCH --cpus-per-task=8
@@ -14,4 +14,4 @@
 ##module load python/3.11-2024.06
 source /project/ls-gruen/users/patrick.gebhardt/envs/gaNdalF/bin/activate
 
-srun -n1 python train_gandalf_classifier.py -cf LMU.cfg -fpn MAG
+srun -n1 python train_gandalf_classifier.py -cf LMU_train_classifier.cfg
