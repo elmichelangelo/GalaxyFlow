5b4ea1483ea2651f32757b985f4c8a1ee929e25a
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-08-28 19:16:55 +0200

    switch training device to CPU, disable GPU allocation, rename `RUN_ID`, and update resource configuration in job script and training settings

--- jobscript_train_flow.sh
+++ jobscript_train_flow.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=gpu_check_optuna_train_flow
+#SBATCH --job-name=cpu_check_optuna_train_flow
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
 #SBATCH --mem=512G
 #SBATCH --chdir=/home/p/P.Gebhardt/development/GalaxyFlow/
-#SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF_train_flow/gandalf_train_flow_%A.log
-#SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF_train_flow/gaNdalF_train_flow_err_%A.log
+#SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF_train_flow/cpu_check_gandalf_train_flow_%A.log
+#SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF_train_flow/cpu_check_gaNdalF_train_flow_err_%A.log
 #SBATCH --partition=inter
-#SBATCH --gres=gpu:a40:1
+#SBATCH --gres=gpu:a40:0
 #SBATCH --cpus-per-task=8
 #SBATCH --signal=B:TERM@300
 
 source /project/ls-gruen/users/patrick.gebhardt/envs/gaNdalF/bin/activate
 
-export RUN_ID="gpu_check_optuna_2025w32"
+export RUN_ID="cpu_check_optuna_2025w32"
 
 srun -n1 python train_gandalf_flow.py -cf LMU_train_flow.cfg
