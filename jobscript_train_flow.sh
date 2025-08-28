110ca9db050c09d93d664ac5026bdcff4ba38c2b
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-08-28 19:13:40 +0200

    update job script and configuration to enable GPU training, adjust resource allocation, and rename `RUN_ID` for consistency

--- jobscript_train_flow.sh
+++ jobscript_train_flow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=cpu_check_optuna_train_flow
+#SBATCH --job-name=gpu_check_optuna_train_flow
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
@@ -8,12 +8,12 @@
 #SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF_train_flow/gandalf_train_flow_%A.log
 #SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF_train_flow/gaNdalF_train_flow_err_%A.log
 #SBATCH --partition=inter
-#SBATCH --gres=gpu:a40:0
+#SBATCH --gres=gpu:a40:1
 #SBATCH --cpus-per-task=8
 #SBATCH --signal=B:TERM@300
 
 source /project/ls-gruen/users/patrick.gebhardt/envs/gaNdalF/bin/activate
 
-export RUN_ID="cpu_check_optuna_2025w32"
+export RUN_ID="gpu_check_optuna_2025w32"
 
 srun -n1 python train_gandalf_flow.py -cf LMU_train_flow.cfg
