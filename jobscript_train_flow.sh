a149f7694af324472d9874e74b388c02e684381c
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-08-28 10:41:08 +0200

    update job script to increase memory allocation, disable GPU usage, rename `RUN_ID`, and adjust training configuration for Optuna optimization

--- jobscript_train_flow.sh
+++ jobscript_train_flow.sh
@@ -3,17 +3,17 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
-#SBATCH --mem=256G
+#SBATCH --mem=512G
 #SBATCH --chdir=/home/p/P.Gebhardt/development/GalaxyFlow/
 #SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF_train_flow/gandalf_train_flow_%A.log
 #SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF_train_flow/gaNdalF_train_flow_err_%A.log
 #SBATCH --partition=inter
-#SBATCH --gres=gpu:a40:1
+#SBATCH --gres=gpu:a40:0
 #SBATCH --cpus-per-task=8
 #SBATCH --signal=B:TERM@300
 
 source /project/ls-gruen/users/patrick.gebhardt/envs/gaNdalF/bin/activate
 
-export RUN_ID="cpu_test_2025w32"
+export RUN_ID="optuna_2025w32"
 
 srun -n1 python train_gandalf_flow.py -cf LMU_train_flow.cfg
