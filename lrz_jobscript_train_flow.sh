1149d1c25779e6bcde5b373314300a0819c42e05
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-09-06 14:29:45 +0200

    start a second run with gpu

--- lrz_jobscript_train_flow.sh
+++ lrz_jobscript_train_flow.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=optuna_train_flow
+#SBATCH --job-name=optuna2_train_flow
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=3:00:00
 #SBATCH --mem=512G
-#SBATCH -o /dss/dsshome1/04/di97tac/logs/optuna_gandalf_train_flow_%A.log
-#SBATCH -e /dss/dsshome1/04/di97tac/logs/optuna_gaNdalF_train_flow_err_%A.log
+#SBATCH -o /dss/dsshome1/04/di97tac/logs/optuna2_gandalf_train_flow_%A.log
+#SBATCH -e /dss/dsshome1/04/di97tac/logs/optuna2_gaNdalF_train_flow_err_%A.log
 #SBATCH --qos=mcml
 #SBATCH --partition=mcml-hgx-h100-94x4
 #SBATCH --cpus-per-task=8
 #SBATCH --gpus-per-task=1
 
-export RUN_ID="optuna_2025w32"
+export RUN_ID="optuna2_2025w32"
 
 srun --container-image='/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/test_container.sqsh' --container-mounts=/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/:/mnt/project -n1 python /dss/dsshome1/04/di97tac/development/GalaxyFlow/train_gandalf_flow.py -cf LRZ_train_flow.cfg
