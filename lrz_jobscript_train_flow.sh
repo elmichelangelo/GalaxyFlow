ac97e3aabde15eee8e37a5b7ff9ef84c4c45c7e3
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-09-12 22:14:59 +0200

    updated LRZ training configurations and job script: increased OPTUNA_RUNS and MAX_TRAILS, updated RUN_ID, reallocated CPU/GPU resources, and adjusted log paths

--- lrz_jobscript_train_flow.sh
+++ lrz_jobscript_train_flow.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=optuna_cut2_train_flow
+#SBATCH --job-name=optuna_w_cuts
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
 #SBATCH --mem=256G
-#SBATCH -o /dss/dsshome1/04/di97tac/logs/optuna_cut2_train_flow_%A.log
-#SBATCH -e /dss/dsshome1/04/di97tac/logs/optuna_cut2_train_flow_err_%A.log
+#SBATCH -o /dss/dsshome1/04/di97tac/logs/optuna_w_cuts_%A.log
+#SBATCH -e /dss/dsshome1/04/di97tac/logs/optuna_w_cuts_err_%A.log
 #SBATCH --qos=mcml
 #SBATCH --partition=mcml-hgx-h100-94x4
-#SBATCH --cpus-per-task=4
-#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus-per-task=2
 
 srun --container-image='/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/test_container.sqsh' --container-mounts=/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/:/mnt/project -n1 python /dss/dsshome1/04/di97tac/development/GalaxyFlow/train_gandalf_flow.py -cf LRZ_train_flow.cfg
