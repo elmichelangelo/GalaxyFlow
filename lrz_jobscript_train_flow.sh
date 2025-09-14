2deb056bb0b81cdaa86f8fa885b866710fcd2ac0
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-09-14 12:04:47 +0200

    updated LRZ training configurations and job script: changed RUN_ID to optuna_new_cuts, enabled GPU usage, switched to mcml partition, and updated resource allocation and log paths

--- lrz_jobscript_train_flow.sh
+++ lrz_jobscript_train_flow.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=optuna_wo_gpu
+#SBATCH --job-name=optuna_new_cuts
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
 #SBATCH --mem=256G
-#SBATCH -o /dss/dsshome1/04/di97tac/logs/optuna_wo_gpu_%A.log
-#SBATCH -e /dss/dsshome1/04/di97tac/logs/optuna_wo_gpu_err_%A.log
-#SBATCH --partition=lrz-cpu
+#SBATCH -o /dss/dsshome1/04/di97tac/logs/optuna_new_cuts_%A.log
+#SBATCH -e /dss/dsshome1/04/di97tac/logs/optuna_new_cuts_err_%A.log
+#SBATCH --qos=mcml
+#SBATCH --partition=mcml-hgx-h100-94x4
 #SBATCH --cpus-per-task=12
-#SBATCH --gpus-per-task=0
+#SBATCH --gpus-per-task=3
 
 srun --container-image='/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/test_container.sqsh' --container-mounts=/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/:/mnt/project -n1 python /dss/dsshome1/04/di97tac/development/GalaxyFlow/train_gandalf_flow.py -cf LRZ_train_flow.cfg
