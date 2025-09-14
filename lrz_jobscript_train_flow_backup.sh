e412d76aaf50674ed33128cadf9c0f6d63e6fffd
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-09-14 12:02:07 +0200

    updated LRZ job scripts: commented out mcml QoS, updated GPU allocation to 0 in train_flow script, and added a backup script with original mcml partition and GPU setup

--- lrz_jobscript_train_flow_backup.sh
+++ lrz_jobscript_train_flow_backup.sh
@@ -6,10 +6,9 @@
 #SBATCH --mem=256G
 #SBATCH -o /dss/dsshome1/04/di97tac/logs/optuna_wo_gpu_%A.log
 #SBATCH -e /dss/dsshome1/04/di97tac/logs/optuna_wo_gpu_err_%A.log
-##SBATCH --qos=mcml
-##SBATCH --partition=mcml-hgx-h100-94x4
-#SBATCH --partition=lrz-cpu
+#SBATCH --qos=mcml
+#SBATCH --partition=mcml-hgx-h100-94x4
 #SBATCH --cpus-per-task=12
-#SBATCH --gpus-per-task=0
+#SBATCH --gpus-per-task=3
 
 srun --container-image='/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/test_container.sqsh' --container-mounts=/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/:/mnt/project -n1 python /dss/dsshome1/04/di97tac/development/GalaxyFlow/train_gandalf_flow.py -cf LRZ_train_flow.cfg
