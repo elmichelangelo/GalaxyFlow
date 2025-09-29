afd018ef576b558a041c37af43af526f1cf88a69
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-09-29 18:58:03 +0200

    updated LRZ training configurations and script: modified file paths, refined input column settings, expanded training and plotting parameters, updated job resource allocations, and adjusted RUN_ID for classifier training consistency

--- jobscript_train_classifier.sh
+++ jobscript_train_classifier.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=classifier_training
+#SBATCH --job-name=cf_training
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
 #SBATCH --mem=128G
 #SBATCH --chdir=/home/p/P.Gebhardt/development/GalaxyFlow/
-#SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF_train_classifer/gandalf_train_classifer_new_data.log
-#SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF_train_classifer/gandalf_train_classifer_new_data_err.log
+#SBATCH -o /home/p/P.Gebhardt/Output/gaNdalF_classifier_training/cf_training_%A.log
+#SBATCH -e /home/p/P.Gebhardt/Output/gaNdalF_classifier_training/cf_training_err_%A.log
 #SBATCH --partition=inter
 #SBATCH --gres=gpu:a40:0
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=12
 
 ##module load python/3.11-2024.06
 source /project/ls-gruen/users/patrick.gebhardt/envs/gaNdalF/bin/activate
