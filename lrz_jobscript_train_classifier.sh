04f90acda58620a014bcaac4f7d362d7be74f40d
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-09-29 19:16:19 +0200

    updated LRZ training script and configurations: modified RUN_ID, adjusted resource allocations (GPUs, CPUs, and partition), reduced EPOCHS, MAX_TRAILS, and OPTUNA_RUNS for streamlined test training scenario

--- lrz_jobscript_train_classifier.sh
+++ lrz_jobscript_train_classifier.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=cf_training
+#SBATCH --job-name=cf_test_training
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
 #SBATCH --mem=256G
-#SBATCH -o /dss/dsshome1/04/di97tac/logs/cf_training_%A.log
-#SBATCH -e /dss/dsshome1/04/di97tac/logs/cf_training_err_%A.log
+#SBATCH -o /dss/dsshome1/04/di97tac/logs/cf_test_training_%A.log
+#SBATCH -e /dss/dsshome1/04/di97tac/logs/cf_test_training_err_%A.log
 #SBATCH --qos=mcml
-#SBATCH --partition=mcml-hgx-h100-94x4
+#SBATCH --partition=mcml-dgx-a100-40x8
 #SBATCH --cpus-per-task=9
-#SBATCH --gpus-per-task=3
+#SBATCH --gpus-per-task=1
 
 srun --container-image='/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/test_container.sqsh' --container-mounts=/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/:/mnt/project -n1 python /dss/dsshome1/04/di97tac/development/GalaxyFlow/train_gandalf_classifier.py -cf LRZ_train_classifier.cfg
