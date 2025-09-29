f746614c8dd04e63ca1eac6ee688debd722bf3f6
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-09-29 22:34:35 +0200

    updated LRZ training script and configurations: updated RUN_ID, adjusted GPUs and partition, and refined logging paths for test training scenario

--- lrz_jobscript_train_flow.sh
+++ lrz_jobscript_train_flow.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=nf_training
+#SBATCH --job-name=nf_test_training
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
 #SBATCH --mem=256G
-#SBATCH -o /dss/dsshome1/04/di97tac/logs/nf_training_%A.log
-#SBATCH -e /dss/dsshome1/04/di97tac/logs/nf_training_err_%A.log
+#SBATCH -o /dss/dsshome1/04/di97tac/logs/nf_test_training_%A.log
+#SBATCH -e /dss/dsshome1/04/di97tac/logs/nf_test_training_err_%A.log
 #SBATCH --qos=mcml
-#SBATCH --partition=mcml-hgx-h100-94x4
+#SBATCH --partition=mcml-dgx-a100-40x8
 #SBATCH --cpus-per-task=12
-#SBATCH --gpus-per-task=3
+#SBATCH --gpus-per-task=1
 
 srun --container-image='/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/gandalf_container.sqsh' --container-mounts=/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/:/mnt/project -n1 python /dss/dsshome1/04/di97tac/development/GalaxyFlow/train_gandalf_flow.py -cf LRZ_train_flow.cfg
