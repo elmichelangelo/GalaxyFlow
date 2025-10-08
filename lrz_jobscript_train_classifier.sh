6b1a1c771cbc49e59cdd0d295896ccf4d7ff36bd
Patrick Gebhardt <patrick.gebhardt@grs.de>
2025-10-08 06:23:42 +0200

    updated LRZ jobscript. Change node and resources

--- lrz_jobscript_train_classifier.sh
+++ lrz_jobscript_train_classifier.sh
@@ -7,8 +7,8 @@
 #SBATCH -o /dss/dsshome1/04/di97tac/logs/cf_new_training_%A.log
 #SBATCH -e /dss/dsshome1/04/di97tac/logs/cf_new_training_err_%A.log
 #SBATCH --qos=mcml
-#SBATCH --partition=mcml-dgx-a100-40x8
-#SBATCH --cpus-per-task=4
-#SBATCH --gpus-per-task=1
+#SBATCH --partition=mcml-hgx-h100-94x4
+#SBATCH --cpus-per-task=12
+#SBATCH --gpus-per-task=3
 
 srun --container-image='/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/gandalf_container.sqsh' --container-mounts=/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/:/mnt/project -n1 python /dss/dsshome1/04/di97tac/development/GalaxyFlow/train_gandalf_classifier.py -cf LRZ_train_classifier.cfg
