ee21af5f295b9839ece359100d7c8b3a130a53af
Patrick Gebhardt <patrick.gebhardt@grs.de>
2026-02-16 10:45:36 +0100

    update job script and LRZ_run_gandalf.cfg: switch to A100 partition, increase CPU allocation, set DEVICE to 'cpu', and adjust related configurations

--- lrz_jobscript_run_gandalf.sh
+++ lrz_jobscript_run_gandalf.sh
@@ -7,8 +7,8 @@
 #SBATCH -o /dss/dsshome1/04/di97tac/logs/run_gandalf_%A.log
 #SBATCH -e /dss/dsshome1/04/di97tac/logs/run_gandalf_err_%A.log
 #SBATCH --qos=mcml
-#SBATCH --partition=mcml-hgx-h100-94x4
-#SBATCH --cpus-per-task=4
+#SBATCH --partition=mcml-dgx-a100-40x8
+#SBATCH --cpus-per-task=12
 #SBATCH --gpus-per-task=1
 
 srun --container-image='/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/gandalf_container.sqsh' --container-mounts=/dss/dssfs02/lwp-dss-0001/pn76fa/pn76fa-dss-0000/di97tac/:/mnt/project -n1 python /dss/dsshome1/04/di97tac/development/GalaxyFlow/run_gaNdalF.py -cf LRZ_run_gandalf.cfg
