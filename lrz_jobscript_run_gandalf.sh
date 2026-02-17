4ec013f2af5654c1ed335ef1ab45c88a53b758a7
Patrick Gebhardt <patrick.gebhardt@grs.de>
2026-02-17 10:48:20 +0100

    update job script: switch to A100 partition and comment out V100 settings

--- lrz_jobscript_run_gandalf.sh
+++ lrz_jobscript_run_gandalf.sh
@@ -6,9 +6,9 @@
 #SBATCH --mem=256G
 #SBATCH -o /dss/dsshome1/04/di97tac/logs/run_gandalf_%A.log
 #SBATCH -e /dss/dsshome1/04/di97tac/logs/run_gandalf_err_%A.log
-##SBATCH --qos=mcml
-##SBATCH --partition=mcml-dgx-a100-40x8
-#SBATCH --partition=lrz-dgx-1-v100x8
+#SBATCH --qos=mcml
+#SBATCH --partition=mcml-dgx-a100-40x8
+##SBATCH --partition=lrz-dgx-1-v100x8
 #SBATCH --cpus-per-task=12
 #SBATCH --gpus-per-task=1
 
